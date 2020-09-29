0f1f1189682d156a1dcec37e54b44135aa06eda3
Username1111111111 <14928811+Username1111111111@users.noreply.github.com>
2020-09-29 15:35:56 +0300

    Added slider

--- js/script.js
+++ js/script.js
@@ -202,8 +202,59 @@ window.addEventListener('DOMContentLoaded', function () {
             .then(formData => postData(formData)) 
             .then(() => statusMessage.innerHTML = message.loading)
             .then(() => statusMessage.innerHTML = message.success)
+            // multiple .then don't change the resolve() result. And it's not chaining
             .catch(() => statusMessage.innerHTML = message.failure)
+            // will be executed anyway if after catch()
             .then(clearInput(target));
+        }
+    });
+
+    // Slider --------------------------------------------------------
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');    
+    
+    function showSlides(n) { 
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((slide) => slide.style.display = 'none');
+        dots.forEach((dot) => dot.classList.remove('dot-active'));
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+    
+    showSlides(slideIndex);
+
+    function plusOrMinusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusOrMinusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusOrMinusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event){
+        for (let i = 1; i <= dots.length; i++) {
+            if (event.target && event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlide(i);
+            }
         }
     });
 
